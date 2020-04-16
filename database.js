0e428c3df9caa0c2a36cb9dcdfa3df3087a67d3d
pixelypants <hudson.graham@gmail.com>
2020-04-16 19:13:37 +1000

    Add password var

--- database.js
+++ database.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
+const pass = "xxx"
 
 const initDB = () => {
-
-  mongoose.connect('mongodb+srv://koa-graphql:<password>@koa-graphql-cluster-g6ygk.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+  mongoose.connect(`mongodb+srv://koa-graphql:${pass}@koa-graphql-cluster-g6ygk.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true });
   mongoose.connection.once('open', () => {
     console.log('connected to database');
   });
-
 }
 
 module.exports = initDB;
