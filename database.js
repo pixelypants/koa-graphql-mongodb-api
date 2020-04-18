42d998a33788b27cc5b42fb2f4392e2620d086a2
pixelypants <hudson.graham@gmail.com>
2020-04-18 16:50:56 +1000

    Add DB debugging

--- database.js
+++ database.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 const initDB = () => {
-  console.log("MongoDB Pass:", process.env.MONGODB_PASS)
+  console.log("MongoD::MONGODB_PASS:", process.env.MONGODB_PASS)
+  console.log("MongoDB::DEBUG:", process.env.DEBUG)
+  
+  if (process.env.DEBUG === "true"){
+    mongoose.set('debug', true);
+  }
+  
   mongoose.connect(`mongodb+srv://koa-graphql:${process.env.MONGODB_PASS}@koa-graphql-cluster-g6ygk.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
   mongoose.connection.once('open', () => {
     console.log('connected to database');
