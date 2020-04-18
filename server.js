ffa44d1f4c3a7f10f6cf38044bd382ea2bc58ae9
pixelypants <hudson.graham@gmail.com>
2020-04-18 15:26:36 +1000

    Add logging

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // https://medium.com/better-programming/how-to-setup-a-powerful-api-with-graphql-koa-and-mongodb-339cfae832a1
 const Koa = require('koa');
 
+const logger = require('koa-logger')
 const mount = require('koa-mount');
 const graphqlHTTP = require('koa-graphql');
 const schema = require('./graphql/schema');
@@ -11,6 +12,7 @@ const initDB = require('./database');
 initDB();
 
 const app = new Koa();
+app.use(logger())
 
 app.listen(9000);
 
@@ -22,3 +24,6 @@ app.use(mount('/graphql', graphqlHTTP({
 app.on('error', err => {
   log.error('server error', err)
 });
+
+console.log('\n > listening on port 9000');
+console.log(' > Navigate to: http://localhost:9000/graphql \n');
