e9cc5338241bdaeaa13872b94999d50a81c1ec22
pixelypants <hudson.graham@gmail.com>
2020-04-18 14:46:52 +1000

    Add dotenv for secrets

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const pass = "xxx"
 
 const initDB = () => {
-  mongoose.connect(`mongodb+srv://koa-graphql:${pass}@koa-graphql-cluster-g6ygk.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true });
+  console.log("MongoDB Pass:", process.env.MONGODB_PASS)
+  mongoose.connect(`mongodb+srv://koa-graphql:${process.env.MONGODB_PASS}@koa-graphql-cluster-g6ygk.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
   mongoose.connection.once('open', () => {
     console.log('connected to database');
   });
--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const Koa = require('koa');
 
 const mount = require('koa-mount');
 const graphqlHTTP = require('koa-graphql');
-
 const schema = require('./graphql/schema');
+
+require('dotenv').config()
 const initDB = require('./database');
 
 initDB();
 
 const app = new Koa();
 
-app.listen(9001);
+app.listen(9000);
 
 app.use(mount('/graphql', graphqlHTTP({
   schema: schema,
